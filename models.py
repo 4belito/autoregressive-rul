0aebaf36ae3c277128ec1cc413791f52b72f221b
acoursey3 <austin2000coursey@gmail.com>
2023-12-04 00:02:35 -0600

    add autoencoder trained and (broken) transition model

--- models.py
+++ models.py
@@ -6,30 +6,30 @@ class TransformerEncoder(nn.Module):
                 input_dim=9,
                 embed_dim=9,
                 dim_feedforward=2048,
+                n_timesteps=10,
                 n_head=1,
                 dropout=0.5,
                 activation="gelu",
                 device='cpu'): #128,256
         super().__init__()
         
-        self.input_projection = nn.Sequential(nn.Flatten(1),nn.Linear(input_dim,embed_dim))
-        self.positional_embed = nn.Parameter(torch.randn(embed_dim)).to(device)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=n_head,dim_feedforward=dim_feedforward, dropout=dropout,activation=activation)
+        self.positional_embed = nn.Parameter(torch.randn(input_dim)).to(device)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=input_dim, nhead=n_head,dim_feedforward=dim_feedforward, dropout=dropout,activation=activation, batch_first=True)
         self.transformer_blocks = nn.TransformerEncoder(self.encoder_layer, num_layers=1) 
-        self.latent_head = nn.Sequential(nn.Linear(embed_dim, embed_dim),
+        self.latent_head = nn.Sequential(nn.Linear(input_dim*n_timesteps, dim_feedforward),
                                         nn.ReLU(),
                                         nn.Dropout(p=dropout),
-                                        nn.Linear(embed_dim, embed_dim),
+                                        nn.Linear(dim_feedforward, dim_feedforward),
                                         nn.ReLU(),
                                         nn.Dropout(p=dropout),
-                                        nn.Linear(embed_dim, embed_dim))
+                                        nn.Linear(dim_feedforward, embed_dim))
     
-    def forward(self, x):     
-        z = self.input_projection(x)
-        z = self.positional_embed + z            
+    def forward(self, x):    
+        z = self.positional_embed + x   
         z = self.transformer_blocks(z)
+        z = z.flatten(1)
         z = self.latent_head(z)
-        return z.squeeze(1)
+        return z
     
 class TransformerDecoder(nn.Module):
     def __init__(self,
@@ -41,25 +41,25 @@ class TransformerDecoder(nn.Module):
                 activation="gelu"): #128,256
         super().__init__()
         
-        self.latent_head = nn.Sequential(nn.Linear(embed_dim, embed_dim),
+        self.latent_head = nn.Sequential(nn.Linear(embed_dim, dim_feedforward),
                                         nn.ReLU(),
                                         nn.Dropout(p=dropout),
-                                        nn.Linear(embed_dim, embed_dim),
+                                        nn.Linear(dim_feedforward, dim_feedforward),
                                         nn.ReLU(),
                                         nn.Dropout(p=dropout),
-                                        nn.Linear(embed_dim, embed_dim))
+                                        nn.Linear(dim_feedforward, embed_dim))
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=n_head,dim_feedforward=dim_feedforward, dropout=dropout,activation=activation)
         self.transformer_blocks = nn.TransformerEncoder(self.encoder_layer, num_layers=1) 
-        self.output_projection = nn.Sequential(nn.Linear(embed_dim, embed_dim),
+        self.output_projection = nn.Sequential(nn.Linear(embed_dim, dim_feedforward),
                                         nn.ReLU(),
                                         nn.Dropout(p=dropout),
-                                        nn.Linear(embed_dim, embed_dim),
+                                        nn.Linear(dim_feedforward, dim_feedforward),
                                         nn.ReLU(),
                                         nn.Dropout(p=dropout),
-                                        nn.Linear(embed_dim, output_dim))
+                                        nn.Linear(dim_feedforward, output_dim))
     
     def forward(self, x):     
         z = self.latent_head(x)           
         z = self.transformer_blocks(z)
         xhat = self.output_projection(z)
-        return xhat.squeeze(1)
+        return xhat
